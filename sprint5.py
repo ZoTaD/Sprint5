c694b8f651ae7c7a9855b27d5c3ccd7c9b0d95a8
thiagozur <105321698+thiagozur@users.noreply.github.com>
2022-07-11 21:04:03 -0300

    5

--- sprint5.py
+++ sprint5.py
@@ -1,5 +1,9 @@
 from decimal import Decimal
 
+def salir(m):
+    print(m)
+    exit()
+
 class Cuenta:
     def __init__(self, lime, limtr, mto, cost, sldd, **kwargs):
         super().__init__(**kwargs)
@@ -22,18 +26,23 @@ class Direccion:
         print(cadena)
 
 class Cliente(Cuenta, Direccion):
-    def __init__(self, lime, limtr, mto, cost, sldd, calle, num, ciudad, prov, pais, nombre, apellido, numc, dni, curtarcred, curcheq):
+    def __init__(self, lime, limtr, mto, cost, sldd, calle, num, ciudad, prov, pais, nombre, apellido, numc, dni, **kwargs):
         super().__init__(lime = lime, limtr = limtr, mto = mto, cost = cost, sldd = sldd, calle = calle, num = num, ciudad = ciudad, prov = prov, pais = pais)
         self.nombre = nombre
         self.apellido = apellido
         self.numero_cliente= numc
         self.dni = dni
-        self.curtarcred = curtarcred
-        self.curcheq = curcheq
 
 class Classic(Cliente):
     def __init__(self, lime, limtr, mto, cost, sldd, calle, num, ciudad, prov, pais, nombre, apellido, numc, dni, curtarcred, curcheq):
-        super().__init__(lime = lime, limtr = limtr, mto = mto, cost = cost, sldd = sldd, calle = calle, num = num, ciudad = ciudad, prov = prov, pais = pais, nombre = nombre, apellido = apellido, numc = numc, dni = dni, curtarcred = curtarcred, curcheq = curcheq)
+        super().__init__(lime = lime, limtr = limtr, mto = mto, cost = cost, sldd = sldd, calle = calle, num = num, ciudad = ciudad, prov = prov, pais = pais, nombre = nombre, apellido = apellido, numc = numc, dni = dni)
+        if curtarcred != 0:
+            salir("Error de cuenta: cantidad de tarjetas de crédito imposible")
+        else: self.curtarcred = curtarcred    
+        if curcheq != 0:
+            salir("Error de cuenta: cantidad de chequeras imposible")
+        else:
+            self.curcheq = curcheq
     def cheq():
         return False
     def tarcred():
@@ -43,14 +52,21 @@ class Classic(Cliente):
 
 class Gold(Cliente):
     def __init__(self, lime, limtr, mto, cost, sldd, calle, num, ciudad, prov, pais, nombre, apellido, numc, dni, curtarcred, curcheq):
-        super().__init__(lime = lime, limtr = limtr, mto = mto, cost = cost, sldd = sldd, calle = calle, num = num, ciudad = ciudad, prov = prov, pais = pais, nombre = nombre, apellido = apellido, numc = numc, dni = dni, curtarcred = curtarcred, curcheq = curcheq)
+        super().__init__(lime = lime, limtr = limtr, mto = mto, cost = cost, sldd = sldd, calle = calle, num = num, ciudad = ciudad, prov = prov, pais = pais, nombre = nombre, apellido = apellido, numc = numc, dni = dni)
+        if curtarcred < 0 or curtarcred > 1:
+            salir("Error de cuenta: cantidad de tarjetas de crédito imposible")
+        else: self.curtarcred = curtarcred    
+        if curcheq < 0 or curcheq > 1:
+            salir("Error de cuenta: cantidad de chequeras imposible")
+        else:
+            self.curcheq = curcheq
     def cheq(self):
-        if self.curcheq >= 1:
+        if self.curcheq == 1:
             return False
         else:
             return True
     def tarcred(self):
-        if self.curtarcred >= 2:
+        if self.curtarcred == 1:
             return False
         else:
             return True
@@ -59,14 +75,21 @@ class Gold(Cliente):
 
 class Black(Cliente):
     def __init__(self, lime, limtr, mto, cost, sldd, calle, num, ciudad, prov, pais, nombre, apellido, numc, dni, curtarcred, curcheq):
-        super().__init__(lime = lime, limtr = limtr, mto = mto, cost = cost, sldd = sldd, calle = calle, num = num, ciudad = ciudad, prov = prov, pais = pais, nombre = nombre, apellido = apellido, numc = numc, dni = dni, curtarcred = curtarcred, curcheq = curcheq)
+        super().__init__(lime = lime, limtr = limtr, mto = mto, cost = cost, sldd = sldd, calle = calle, num = num, ciudad = ciudad, prov = prov, pais = pais, nombre = nombre, apellido = apellido, numc = numc, dni = dni)
+        if curtarcred < 0 or curtarcred > 5:
+            salir("Error de cuenta: cantidad de tarjetas de crédito imposible")
+        else: self.curtarcred = curtarcred    
+        if curcheq < 0 or curcheq > 2:
+            salir("Error de cuenta: cantidad de chequeras imposible")
+        else:
+            self.curcheq = curcheq
     def cheq(self):
-        if self.curcheq >= 2:
+        if self.curcheq == 2:
             return False
         else:
             return True
     def tarcred(self):
-        if self.curtarcred >= 5:
+        if self.curtarcred == 5:
             return False
         else:
             return True
