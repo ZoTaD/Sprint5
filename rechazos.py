33768fc383defb25a2df9ea6a415b33053271475
thiagozur <105321698+thiagozur@users.noreply.github.com>
2022-07-17 11:57:42 -0300

    9

--- rechazos.py
+++ rechazos.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
 
 class Decline:
-    def __init__(self, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
+    def __init__(self, estado, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
+        self.state = estado
         self.t = tipo
         self.cn = cuenta
         self.remaining = Decimal(cupor)
@@ -12,51 +13,82 @@ class Decline:
         self.cliente = cliente
     def solve(self):
         print("Debe crearse un objeto del tipo de rechazo adecuado para poder utilizarse este método")
+        return "Debe crearse un objeto del tipo de rechazo adecuado para poder utilizarse este método"
 
 class DecAltaChequera(Decline):
-    def __init__(self, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
-        super().__init__(tipo, cuenta, cupor, monto, date, num, saldo, cliente)
+    def __init__(self, estado, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
+        super().__init__(tipo, estado, cuenta, cupor, monto, date, num, saldo, cliente)
     def solve(self):
         if not self.cliente.cheq():
             print("Transacción rechazada debido a que la cantidad de chequeras permitidas para esta cuenta ha sido alcanzada")
+            return "Transacción rechazada debido a que la cantidad de chequeras permitidas para esta cuenta ha sido alcanzada"
+        else:
+                print("Transacción aceptada")
+                return "Transacción aceptada"
 
 class DecAltaTarcred(Decline):
-    def __init__(self, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
-        super().__init__(tipo, cuenta, cupor, monto, date, num, saldo, cliente)
+    def __init__(self, estado, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
+        super().__init__(tipo, estado, cuenta, cupor, monto, date, num, saldo, cliente)
     def solve(self):
         if not self.cliente.tarcred():
             print("Transacción rechazada debido a que a cantidad de tarjetas de crédito permitidas para esta cuenta ha sido alcanzada")
+            return "Transacción rechazada debido a que a cantidad de tarjetas de crédito permitidas para esta cuenta ha sido alcanzada"
+        else:
+                print("Transacción aceptada")
+                return "Transacción aceptada"
 
 class DecDolar(Decline):
-    def __init__(self, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
-        super().__init__(tipo, cuenta, cupor, monto, date, num, saldo, cliente)
+    def __init__(self, estado, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
+        super().__init__(tipo, estado, cuenta, cupor, monto, date, num, saldo, cliente)
     def solve(self):
         if not self.cliente.dolar():
             print("Transacción rechazada debido a que las cuentas classic no pueden comprar dólares")
+            return "Transacción rechazada debido a que las cuentas classic no pueden comprar dólares"
         else:
             if self.m > self.s:
                 print("Transacción rechazada por monto mayor a saldo en cuenta")
+                return "Transacción rechazada por monto mayor a saldo en cuenta"
+            else:
+                print("Transacción aceptada")
+                return "Transacción aceptada"
+                    
 
 class DecRetEfec(Decline):
-    def __init__(self, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
-        super().__init__(tipo, cuenta, cupor, monto, date, num, saldo, cliente)
+    def __init__(self, estado, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
+        super().__init__(tipo, estado, cuenta, cupor, monto, date, num, saldo, cliente)
     def solve(self):
         if (self.s + self.cliente.account.saldo_descubierto_disponible) < self.m:  
             print("Transacción rechazada por monto mayor a saldo en cuenta")
+            return "Transacción rechazada por monto mayor a saldo en cuenta"
         elif self.m > self.remaining:
-            print("Transacción rechazada por monto mayor a límite de extracción diario")    
+            print("Transacción rechazada por monto mayor a límite de extracción diario")
+            return "Transacción rechazada por monto mayor a límite de extracción diario"
+        else:
+            print("Transacción aceptada")
+            return "Transacción aceptada"
 
 class DecTranfRec(Decline):
-    def __init__(self, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
-        super().__init__(tipo, cuenta, cupor, monto, date, num, saldo, cliente)
+    def __init__(self, estado, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
+        super().__init__(tipo, estado, cuenta, cupor, monto, date, num, saldo, cliente)
     def solve(self):
         if self.cliente.account.limite_transferencia_recibida:
             if self.cliente.account.limite_transferencia_recibida <= self.m:
                 print("Transacción rechazada debido a que el monto es mayor al límite de transferencias recibidas para las cuentas de tipo", self.cliente.__class__.__name__)
+                return "Transacción rechazada debido a que el monto es mayor al límite de transferencias recibidas para las cuentas de tipo", self.cliente.__class__.__name__
+            else:
+                print("Transacción aceptada")
+                return "Transacción aceptada"
+        else:
+            print("Transacción aceptada")
+            return "Transacción aceptada"
 
 class DecTranfEnv(Decline):
-    def __init__(self, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
-        super().__init__(tipo, cuenta, cupor, monto, date, num, saldo, cliente)
+    def __init__(self, estado, tipo, cuenta, cupor, monto, date, num, saldo, cliente):
+        super().__init__(tipo, estado, cuenta, cupor, monto, date, num, saldo, cliente)
     def solve(self):
         if (self.m + self.m * self.cliente.account.costo_transferencias) > (self.s + self.cliente.account.saldo_descubierto_disponible):
             print("Transacción rechazada debido a que el monto y la comisión (de aplicarse) son mayores al saldo en cuenta (incluyendo descubierto en caso de tenerse)")
+            return "Transacción rechazada debido a que el monto y la comisión (de aplicarse) son mayores al saldo en cuenta (incluyendo descubierto en caso de tenerse)"
+        else:
+            print("Transacción aceptada")
+            return "Transacción aceptada"
--- sprint5.py
+++ sprint5.py
@@ -1,4 +1,6 @@
-import json, cuentas, rechazos, sys
+import json, cuentas, rechazos, sys, codecs
+from simple_html.nodes import body, head, html, p, tr, td, tbody, th
+from simple_html.render import render
 
 print("")
 
@@ -35,7 +37,6 @@ elif arcparse["tipo"] == "BLACK":
 
 for i in transacciones:
     try:
-        if i["estado"] == "RECHAZADA":
             c.account.monto = i["saldoEnCuenta"]
 
             c.curtarcred = i["totalTarjetasDeCreditoActualmente"]
@@ -43,26 +44,57 @@ for i in transacciones:
             c.curcheq = i["totalChequerasActualmente"]
 
             if i["tipo"] == "ALTA_CHEQUERA":
-                rejectedt[f"tr {i['numero']}"] = rechazos.DecAltaChequera(i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
+                rejectedt[f"tr {i['numero']}"] = rechazos.DecAltaChequera(i["estado"], i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
             elif i["tipo"] == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO":
-                rejectedt[f"tr {i['numero']}"] = rechazos.DecRetEfec(i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
+                rejectedt[f"tr {i['numero']}"] = rechazos.DecRetEfec(i["estado"], i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
             elif i["tipo"] == "ALTA_TARJETA_CREDITO":
-                rejectedt[f"tr {i['numero']}"] = rechazos.DecAltaTarcred(i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
+                rejectedt[f"tr {i['numero']}"] = rechazos.DecAltaTarcred(i["estado"], i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
             elif i["tipo"] == "COMPRA_DOLAR":
-                rejectedt[f"tr {i['numero']}"] = rechazos.DecDolar(i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
+                rejectedt[f"tr {i['numero']}"] = rechazos.DecDolar(i["estado"], i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
             elif i["tipo"] == "TRANSFERENCIA_RECIBIDA":
-                rejectedt[f"tr {i['numero']}"] = rechazos.DecTranfRec(i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
+                rejectedt[f"tr {i['numero']}"] = rechazos.DecTranfRec(i["estado"], i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
             elif i["tipo"] == "TRANSFERENCIA_ENVIADA":
-                rejectedt[f"tr {i['numero']}"] = rechazos.DecTranfEnv(i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
+                rejectedt[f"tr {i['numero']}"] = rechazos.DecTranfEnv(i["estado"], i["tipo"], i["cuentaNumero"], i["cupoDiarioRestante"], i["monto"], i["fecha"], i["numero"], i["saldoEnCuenta"], c)
             print("Transacción", rejectedt[f"tr {i['numero']}"].tnum)
             rejectedt[f"tr {i['numero']}"].solve()
             print("-------")
-        else:
-            print("La transacción", i["numero"], "se encuentra aceptada")
-            print("-------")
+        
     except:
         cuentas.salir("Error de lectura del archivo JSON al recorrer el listado de transacciones")
 
 arc.close()
 
 print("")
+
+clientname=f"<h1>Transacciones de: {nombre} {apellido}</h1>"
+htmllines=""
+
+for i in rejectedt.values():
+    node = tr(
+    th.attrs(scope="row")(
+        f"{i.tnum}"
+    ),
+    td(
+        f"{i.t}"
+    ),
+    td(
+        f"{i.date}"
+    ),
+    td(
+        f"{i.state}"
+    ),
+    td(
+        f"{i.solve()}"
+    )
+    )
+    htmllines += f"              {render(node)}\n"
+
+tablatemp = codecs.open("./files/template.html", "r", "utf-8")
+filelines = tablatemp.readlines()
+filelines[22] = htmllines
+filelines[9] = clientname
+tablatemp.close()
+
+tabla = codecs.open("prueba.html", "w", "utf-8")
+tabla.writelines(filelines)
+tabla.close()
